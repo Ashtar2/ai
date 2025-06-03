9c25e331c9d0e52d482de35ff602301390c92d42
Аштар Аштаров <astarastarov@MacBook-Pro-2.local>
2025-06-03 22:04:40 +0300

    None

--- main.py
+++ main.py
@@ -32,5 +32,5 @@ async def health_check():
     return {"status": "ok"}
 
 
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
